4dbe195e4541397f8aa0e6288ec7508e5b106321
Евгений <ejigura@mail.ru>
2024-02-25 20:11:16 +0300

    BetaRegistrationPOST2

--- BeeClient/BeeClient.Client/Data/CompanyData.cs
+++ BeeClient/BeeClient.Client/Data/CompanyData.cs
@@ -1,27 +1,39 @@
 ﻿using BeeClient.Client.Entities.Models.DTos;
 using BeeClient.Client.Entities.Models;
+using Blazored.LocalStorage;
 using System.Net;
+using System.Reflection;
 
-namespace BeeClient.Client.Data
+namespace BeeClient.Client.Data;
+
+public class CompanyData
 {
-    public class CompanyData
+    private readonly DataSender sender;
+
+    public CompanyData(DataSender sender)
     {
-        private readonly DataSender sender;
-
-        public CompanyData(DataSender sender)
-        {
-            this.sender = sender;
-        }
-        public async Task<Result<Comapny>> Register(CreateCompany company)
-        {
-            return await sender.Post<Company, CreateCompany>(company, "companies/new");
-        }
-
+        this.sender = sender;
+    }
+    public async Task<Result<Company>> RegisterAsync(CreateCompany company)
+    {
+        // Выполняем POST-запрос на сервер для регистрации компании
+        return await sender.Post<Company, CreateCompany>(company, "companies/new");
     }
 
-    // для превращения json в класс можешь использовать например https://json2csharp.com/
-    public class Company
-    {
-        public int MyProperty { get; set; }
-    }
+}
+
+
+
+// для превращения json в класс можешь использовать например https://json2csharp.com/
+public class Company
+{
+    public string СompanyName { get; set; }
+    public string Inn { get; set; }
+    public string PhNumber { get; set; }
+    public string CompanyTag { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
+    public string Experience { get; set; }
+    public string IsActive { get; set; }
+
 }
--- BeeClient/BeeClient.Client/Extensions/ServiceExtensions.cs
+++ BeeClient/BeeClient.Client/Extensions/ServiceExtensions.cs
@@ -21,7 +21,7 @@ public static class ServiceExtensions
         services.AddScoped<DataSender>();
         services.AddScoped<TokenData>();
         services.AddScoped<UserData>();
-
+        services.AddScoped<CompanyData>();
 
         services.AddBlazoredLocalStorage();
     }
